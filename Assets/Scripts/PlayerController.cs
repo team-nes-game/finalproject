b59fd5a8bdd64e270cc68d11992649177c1b1378
Sam <swsvindland@gmail.com>
2019-05-15 23:52:49 -0700

    Hub World Made

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Vector3 movement;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement = Vector3.zero;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if (movement != Vector3.zero)
+        {
+            rb.MovePosition(transform.position + movement * speed * Time.deltaTime);
+            //animator.SetFloat("MoveX", movement.x);
+            //animator.SetFloat("MoveY", movement.y);
+            //animator.SetBool("Walking", true);
+        }
+        else
+        {
+            //animator.SetBool("Walking", false);
+        }
+    }
+}
